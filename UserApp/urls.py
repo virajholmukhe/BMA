6302a900131d387ac659cbd41d68c30547079e2b
Viraj <virajholmukhe@gmail.com>
2021-08-14 19:02:39 +0530

    test3 commit

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -2,15 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.home),
+    path('',views.home),
     path('view_all_idols_list',views.showAllIdols),
     path('categories_list/<cid>',views.showCategorywiseIdols),
     path('view_idol/<pid>',views.showIdol),
     path('search_text',views.search),
 
-
-
-    path('',views.test),
 
 ]
 
