41e33d5570a7acea0e76ba562ba117d496d68151
Viraj Holmukhe <virajholmukhe.com>
2022-08-10 23:02:55 +0530

    Final

--- UserApp/views.py
+++ UserApp/views.py
@@ -15,33 +15,40 @@ def home(request):
         obj.save()
         return redirect(home)
     else:
-        cat = Categories.objects.filter(~Q(id=3))
-        cats = Categories.objects.filter(~Q(id=3))
-        idol = Idol.objects.filter(featured=1)
+        #cat = Categories.objects.filter(~Q(id=3))
+        cat = Categories.objects.filter(id=6)
+        #cats = Categories.objects.filter(~Q(id=3))
+        cats = Categories.objects.filter(id=6)
+        idol = Idol.objects.filter(featured=2)
         return render(request,'UserApp/home.html',{"cat":cat,"idol":idol,"cats":cats})
 
 def showCategorywiseIdols(request,cid):
-    cat = Categories.objects.filter(~Q(id=cid))
-    cats = Categories.objects.filter(~Q(id=3))
+    #cat = Categories.objects.filter(~Q(id=cid))
+    cat = Categories.objects.filter(id=6)
+    #cats = Categories.objects.filter(~Q(id=3))
+    cats = Categories.objects.filter(id=6)
     idol = Idol.objects.filter(category_id=cid).order_by("-id")
     obj = Categories.objects.get(id=cid)
     cat_name = obj.name
     return render(request,"UserApp/view_idols_list.html",{"cat":cat,"idol":idol,"cats":cats,"cat_name":cat_name})
 
 def showAllIdols(request):
-    cats = Categories.objects.filter(~Q(id=3))
-    idol = Idol.objects.all().order_by("-id")
+    #cats = Categories.objects.filter(~Q(id=3))
+    cats = Categories.objects.filter(id=6)
+    idol = Idol.objects.filter(category_id=6).order_by("-id")
     return render(request,"UserApp/view_idols_list.html",{"idol":idol,"cats":cats})
 
 def search(request):
     sq = request.GET["sq"]
     query = (Q(model_name__icontains = sq) | Q(model_no__icontains = sq))
     idol = Idol.objects.filter(query)
-    cats = Categories.objects.filter(~Q(id=3))
+    #cats = Categories.objects.filter(~Q(id=3))
+    cats = Categories.objects.filter(id=6)
     return render(request,"UserApp/view_idols_list.html",{"idol":idol,"cats":cats})
 
 def showIdol(request,pid):
-    cats = Categories.objects.filter(~Q(id=3))
+    #cats = Categories.objects.filter(~Q(id=3))
+    cats = Categories.objects.filter(id=6)
     idol = Idol.objects.get(id=pid)
     cat_id = idol.category_id
     idols = Idol.objects.filter(category_id=cat_id)
